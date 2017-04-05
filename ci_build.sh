ede2d8169aa987a5c445312ae37e46445eef9adb
Jim Klimov <jim@jimklimov.com>
2017-04-05 14:34:15 +0200

    ci_build.sh : tell Travis to try building everything so we see all errors

--- ci_build.sh
+++ ci_build.sh
@@ -425,6 +425,7 @@ esac
         $CI_TIME make VERBOSE=1 memcheck
         exit $?
     fi
+    $CI_TIME make VERBOSE=1 -k all || \
     $CI_TIME make VERBOSE=1 all
 
     echo "=== Are GitIgnores good after 'make all' with drafts? (should have no output below)"
@@ -450,6 +451,7 @@ esac
     (
         $CI_TIME ./autogen.sh 2> /dev/null
         $CI_TIME ./configure --enable-drafts=no "${CONFIG_OPTS[@]}" --with-docs=yes
+        $CI_TIME make VERBOSE=1 -k all || \
         $CI_TIME make VERBOSE=1 all || exit $?
         (
             export DISTCHECK_CONFIGURE_FLAGS="--enable-drafts=no ${CONFIG_OPTS[@]} --with-docs=yes" && \
