42ity/fty-nut

commit 96d189fa064f448205c962e22384b99dc70613ec

Simplify malamute connection setup
Pass the endpoint name as an argument to the actor function and move the
stream setup logic to the actors themselves. This not only reduces code
size, but also makes sure that we enter the main loop with a workable
malamute connection. While at it, fix a few memory leaks in error paths.

src/alert_actor.cc
@@ -20,7 +20,7 @@
 */
 #include "alert_device_list.h"
 #include "alert_actor.h"
-#include "fty_nut.h"
+#include "nut_mlm.h"
 #include "logger.h"
 
 #include <malamute.h>
@@ -60,85 +60,6 @@ alert_actor_commands (
     else
     if (streq (cmd, "VERBOSE")) {
         verbose = true;
-    }
-    else
-    if (streq (cmd, ACTION_CONNECT)) {
-        char *endpoint = zmsg_popstr (message);
-        if (!endpoint) {
-            log_error (
-                    "aa: Expected multipart string format: CONNECT/endpoint/name. "
-                    "Received CONNECT/nullptr");
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        char *name = zmsg_popstr (message);
-        if (!name) {
-            log_error (
-                    "aa: Expected multipart string format: CONNECT/endpoint/name. "
-                    "Received CONNECT/endpoint/nullptr");
-            zstr_free (&endpoint);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_connect (client, endpoint, 1000, name);
-        if (rv == -1) {
-            log_error (
-                    "aa: mlm_client_connect (endpoint = '%s', timeout = '%d', address = '%s') failed",
-                    endpoint, 1000, name);
-        }
-        if (mb_client != NULL) {
-            char *mb_name = zsys_sprintf ("%s-mb", name);
-            rv = mlm_client_connect (mb_client, endpoint, 1000, mb_name);
-            if (rv == -1) {
-                log_error (
-                        "aa: mlm_client_connect (endpoint = '%s', timeout = '%d', address = '%s') failed",
-                        endpoint, 1000, mb_name);
-            }
-            zstr_free (&mb_name);
-        }
-        zstr_free (&endpoint);
-        zstr_free (&name);
-    }
-    else
-    if (streq (cmd, ACTION_PRODUCER)) {
-        char *stream = zmsg_popstr (message);
-        if (!stream) {
-            log_error (
-                    "aa: Expected multipart string format: PRODUCER/stream. "
-                    "Received PRODUCER/nullptr");
-            zstr_free (&stream);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_set_producer (client, stream);
-        if (rv == -1) {
-            log_error ("mlm_client_set_producer (stream = '%s') failed", stream);
-        }
-        zstr_free (&stream);
-    }
-    else
-    if (streq (cmd, ACTION_CONSUMER)) {
-        char *stream = zmsg_popstr (message);
-        char *pattern = zmsg_popstr (message);
-        if (!stream || !pattern) {
-            log_error (
-                    "aa: Expected multipart string format: CONSUMER/stream/pattern. "
-                    "Received PRODUCER/%s/%s", stream ? stream : "nullptr", pattern ? pattern : "nullptr");
-            zstr_free (&stream);
-            zstr_free (&pattern);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_set_consumer (client, stream, pattern);
-        if (rv == -1) {
-            log_error ("mlm_client_set_consumer (stream = '%s', pattern = '%s') failed", stream, pattern);
-        }
-        zstr_free (&stream);
-        zstr_free (&pattern);
     }
     else
     if (streq (cmd, ACTION_POLLING)) {
@@ -193,25 +114,40 @@ alert_actor (zsock_t *pipe, void *args)
 
     uint64_t polling = 30000;
     bool verbose = false;
+    const char *endpoint = static_cast<const char *>(args);
 
-    mlm_client_t *client = mlm_client_new ();
+    MlmClientGuard client(mlm_client_new());
     if (!client) {
         log_critical ("mlm_client_new () failed");
         return;
     }
-     mlm_client_t *mb_client = mlm_client_new ();
-     if (!mb_client) {
-        log_critical ("mlm_client_new () failed");
+    if (mlm_client_connect(client, endpoint, 5000, ACTOR_ALERT_NAME) < 0) {
+        log_error("client %s failed to connect", ACTOR_ALERT_NAME);
         return;
-     }
+    }
+    if (mlm_client_set_producer(client, FTY_PROTO_STREAM_ALERTS_SYS) < 0) {
+        log_error("mlm_client_set_producer (stream = '%s') failed",
+                FTY_PROTO_STREAM_ALERTS_SYS);
+        return;
+    }
+    if (mlm_client_set_consumer(client, FTY_PROTO_STREAM_ASSETS, ".*") < 0) {
+        log_error("mlm_client_set_consumer (stream = '%s', pattern = '.*') failed",
+                FTY_PROTO_STREAM_ASSETS);
+        return;
+    }
+
+    MlmClientGuard mb_client(mlm_client_new());
+    if (!mb_client) {
+       log_critical ("mlm_client_new () failed");
+       return;
+    }
 
     Devices devices;
     devices.setPollingMs (polling);
 
-    zpoller_t *poller = zpoller_new (pipe, mlm_client_msgpipe (client), NULL);
+    ZpollerGuard poller(zpoller_new(pipe, mlm_client_msgpipe(client), NULL));
     if (!poller) {
         log_critical ("zpoller_new () failed");
-        mlm_client_destroy (&client);
         return;
     }
     zsock_signal (pipe, 0);
@@ -259,9 +195,6 @@ alert_actor (zsock_t *pipe, void *args)
             zmsg_destroy (&msg);
         }
     }
-    zpoller_destroy (&poller);
-    mlm_client_destroy (&mb_client);
-    mlm_client_destroy (&client);
 }
 
 //  --------------------------------------------------------------------------

src/nut_mlm.h
@@ -1,0 +1,63 @@
+/*  =========================================================================
+    fty-nut - Malamute connection defaults
+
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    =========================================================================
+*/
+
+#ifndef NUT_MLM_H_INCLUDED
+#define NUT_MLM_H_INCLUDED
+
+#include <malamute.h>
+#include <czmq.h>
+
+#define MLM_ENDPOINT "ipc://@/malamute"
+// zactor_new takes a void* pointer
+#define MLM_ENDPOINT_VOID (const_cast<void *>(static_cast<const void *>(MLM_ENDPOINT)))
+#define ACTOR_NUT_NAME "fty-nut"
+#define ACTOR_ALERT_NAME "bios-nut-alert"
+#define ACTOR_SENSOR_NAME "agent-nut-sensor"
+
+#define CONFIG_POLLING "nut/polling_interval"
+#define ACTION_POLLING "POLLING"
+#define ACTION_CONFIGURE "CONFIGURE"
+
+// C++ wrapper for czmq / malamute objects with automatic cleanup
+template<typename T, void (*destructor)(T**)>
+class MlmObjGuard
+{
+public:
+    explicit MlmObjGuard(T* ptr)
+        : ptr_(ptr)
+    {}
+    MlmObjGuard(const MlmObjGuard&) = delete;
+    ~MlmObjGuard()
+    {
+        destructor(&ptr_);
+    }
+    operator T*()
+    {
+        return ptr_;
+    }
+private:
+    T* ptr_;
+};
+
+typedef MlmObjGuard<mlm_client_t, mlm_client_destroy> MlmClientGuard;
+typedef MlmObjGuard<zpoller_t, zpoller_destroy> ZpollerGuard;
+
+#endif

Makefile.am
@@ -75,6 +75,7 @@ EXTRA_DIST += \
     src/sensor_device.h \
     src/sensor_list.h \
     src/state_manager.h \
+    src/nut_mlm.h \
     LICENSE \
     README.md \
     CONTRIBUTING.md \

include/fty_nut.h
@@ -25,11 +25,4 @@
 //  Include the project library file
 #include "fty_nut_library.h"
 
-// TODO: Move this to more specific header files
-#define CONFIG_POLLING "nut/polling_interval"
-#define ACTION_POLLING "POLLING"
-#define ACTION_CONNECT "CONNECT"
-#define ACTION_PRODUCER "PRODUCER"
-#define ACTION_CONSUMER "CONSUMER"
-#define ACTION_CONFIGURE "CONFIGURE"
 #endif

project.xml
@@ -74,6 +74,7 @@
             <add_config_opts>--with-dmfsnmp-regenerate=no</add_config_opts>
     </use>
 
+    <header name = "nut_mlm" private = "1">Malamute connection helpers</header>
     <class name = "logger"          private = "1">logging api</class>
     <class name = "fsutils"         private = "1">filesystem utils</class>
     <class name = "cidr"            private = "1">C++ Wrapper around libcidr</class>

src/actor_commands.cc
@@ -28,7 +28,7 @@
 
 #include "actor_commands.h"
 #include "nut_agent.h"
-#include "fty_nut.h"
+#include "nut_mlm.h"
 #include "logger.h"
 
 int
@@ -63,87 +63,6 @@ actor_commands (
     else
     if (streq (cmd, "VERBOSE")) {
         verbose = true;
-    }
-    else
-    if (streq (cmd, ACTION_CONNECT)) {
-        char *endpoint = zmsg_popstr (message);
-        if (!endpoint) {
-            log_error (
-                    "Expected multipart string format: CONNECT/endpoint/name. "
-                    "Received CONNECT/nullptr");
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        char *name = zmsg_popstr (message);
-        if (!name) {
-            log_error (
-                    "Expected multipart string format: CONNECT/endpoint/name. "
-                    "Received CONNECT/endpoint/nullptr");
-            zstr_free (&endpoint);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_connect (client, endpoint, 1000, name);
-        if (rv == -1) {
-            log_error (
-                    "mlm_client_connect (endpoint = '%s', timeout = '%d', address = '%s') failed",
-                    endpoint, 1000, name);
-        }
-        else {
-            nut_agent.setClient (client);
-        }
-        zstr_free (&endpoint);
-        zstr_free (&name);
-    }
-    else
-    if (streq (cmd, "PRODUCER")) {
-        char *stream = zmsg_popstr (message);
-        if (!stream) {
-            log_error (
-                    "Expected multipart string format: PRODUCER/stream. "
-                    "Received PRODUCER/nullptr");
-            zstr_free (&stream);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_set_producer (client, stream);
-        if (rv == -1) {
-            log_error ("mlm_client_set_producer (stream = '%s') failed", stream);
-        }
-        zstr_free (&stream);
-    }
-    else
-    if (streq (cmd, "CONSUMER")) {
-        char *stream = zmsg_popstr (message);
-        if (!stream) {
-            log_error (
-                    "Expected multipart string format: CONSUMER/stream/pattern. "
-                    "Received CONSUMER/nullptr");
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        char *pattern = zmsg_popstr (message);
-        if (!pattern) {
-            log_error (
-                    "Expected multipart string format: CONSUMER/stream/pattern. "
-                    "Received CONSUMER/stream/nullptr");
-            zstr_free (&stream);
-            zstr_free (&cmd);
-            zmsg_destroy (message_p);
-            return 0;
-        }
-        int rv = mlm_client_set_consumer (client, stream, pattern);
-        if (rv == -1) {
-            log_error (
-                    "mlm_client_set_consumer (stream = '%s', pattern = '%s') failed",
-                    stream, pattern);
-        }
-        zstr_free (&pattern);
-        zstr_free (&stream);
     }
     else
     if (streq (cmd, "CONFIGURE")) {
@@ -248,6 +167,7 @@ actor_commands_test (bool verbose)
 
     mlm_client_t *client = mlm_client_new ();
     assert (client);
+    assert(mlm_client_connect(client, endpoint, 5000, "test-agent") == 0);
 
     zmsg_t *message = NULL;
     bool actor_verbose = false;
@@ -268,7 +188,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -286,7 +205,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -304,7 +222,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -324,7 +241,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -344,126 +260,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // CONNECT - expected fail
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONNECT);
-    zmsg_addstr (message, endpoint);
-    // missing name here
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // CONNECT - expected fail
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONNECT);
-    // missing endpoint here
-    // missing name here
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // CONNECT - expected fail; bad endpoint
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONNECT);
-    zmsg_addstr (message, "ipc://fty-nut-server-BAD");
-    zmsg_addstr (message, "test-agent");
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // PRODUCER - expected fail
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_PRODUCER);
-    // missing stream here
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // CONSUMER - expected fail
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONSUMER);
-    zmsg_addstr (message, "some-stream");
-    // missing pattern here
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-    assert (state_file.empty ());
-
-    STDERR_NON_EMPTY
-
-    // --------------------------------------------------------------
-    fp = freopen ("stderr.txt", "w+", stderr);
-    // CONSUMER - expected fail
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONSUMER);
-    // missing stream here
-    // missing pattern here
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == false);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -482,7 +278,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -501,7 +296,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == false);
     assert (actor_polling == 30000);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -535,7 +329,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == true);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == false);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file.empty ());
 
@@ -551,7 +344,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == true);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == false);
     assert (nut_agent.TTL () == 60);
     assert (state_file == "src/selftest_state_file");
 
@@ -565,51 +357,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == true);
     assert (actor_polling == 0);
     assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == false);
-    assert (nut_agent.TTL () == 60);
-
-    // CONNECT
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONNECT);
-    zmsg_addstr (message, endpoint);
-    zmsg_addstr (message, "test-agent");
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == true);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == true);
-    assert (nut_agent.TTL () == 60);
-
-    // CONSUMER
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_CONSUMER);
-    zmsg_addstr (message, "some-stream");
-    zmsg_addstr (message, ".+@.+");
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == true);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == true);
-    assert (nut_agent.TTL () == 60);
-
-    // PRODUCER
-    message = zmsg_new ();
-    assert (message);
-    zmsg_addstr (message, ACTION_PRODUCER);
-    zmsg_addstr (message, "some-stream");
-    rv = actor_commands (client, &message, actor_verbose, actor_polling, nut_agent, data, state_file);
-    assert (rv == 0);
-    assert (message == NULL);
-    assert (actor_verbose == true);
-    assert (actor_polling == 0);
-    assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == true);
     assert (nut_agent.TTL () == 60);
 
     // POLLING
@@ -623,7 +370,6 @@ actor_commands_test (bool verbose)
     assert (actor_verbose == true);
     assert (actor_polling == 150000);
     assert (nut_agent.isMappingLoaded () == true);
-    assert (nut_agent.isClientSet () == true);
     assert (nut_agent.TTL () == 300);
 
     STDERR_EMPTY

src/fty_nut.cc
@@ -30,7 +30,7 @@
 #include "sensor_actor.h"
 #include "alert_actor.h"
 #include "ftyproto.h"
-#include "fty_nut.h"
+#include "nut_mlm.h"
 #include "logger.h"
 
 #include <getopt.h>
@@ -39,11 +39,6 @@
 #include <string>
 
 #define str(x) #x
-
-static const char *ACTOR_NUT_NAME = "fty-nut";
-static const char *ACTOR_ALERT_NAME = "bios-nut-alert";
-static const char *ACTOR_SENSOR_NAME = "agent-nut-sensor";
-static const char *ENDPOINT = "ipc://@/malamute";
 
 #define DEFAULT_LOG_LEVEL LOG_WARNING
 
@@ -195,19 +190,19 @@ int main(int argc, char *argv []) {
 
     log_info("fty_nut - NUT (Network UPS Tools) wrapper/daemon");
 
-    zactor_t *nut_server = zactor_new(fty_nut_server, (void *) NULL);
+    zactor_t *nut_server = zactor_new(fty_nut_server, MLM_ENDPOINT_VOID);
     if (!nut_server) {
         log_critical("zactor_new (task = 'fty_nut_server', args = 'NULL') failed");
         return -1;
     }
 
-    zactor_t *nut_device_alert = zactor_new(alert_actor, (void *) NULL);
+    zactor_t *nut_device_alert = zactor_new(alert_actor, MLM_ENDPOINT_VOID);
     if (!nut_device_alert) {
         log_critical("zactor_new (task = 'nut_device_server', args = 'NULL') failed");
         return -1;
     }
 
-    zactor_t *nut_sensor = zactor_new(sensor_actor, (void *) NULL);
+    zactor_t *nut_sensor = zactor_new(sensor_actor, MLM_ENDPOINT_VOID);
     if (!nut_sensor) {
         log_critical("zactor_new (task = 'nut_sensor', args = 'NULL') failed");
         return -1;
@@ -220,19 +215,10 @@ int main(int argc, char *argv []) {
     }
     zstr_sendx(nut_server, ACTION_CONFIGURE, mapping_file.c_str(), state_file.c_str(), NULL);
     zstr_sendx(nut_server, ACTION_POLLING, polling, NULL);
-    zstr_sendx(nut_server, ACTION_CONNECT, ENDPOINT, ACTOR_NUT_NAME, NULL);
-    zstr_sendx(nut_server, ACTION_PRODUCER, FTY_PROTO_STREAM_METRICS, NULL);
-    zstr_sendx(nut_server, ACTION_CONSUMER, FTY_PROTO_STREAM_ASSETS, ".*", NULL);
 
     zstr_sendx(nut_device_alert, ACTION_POLLING, polling, NULL);
-    zstr_sendx(nut_device_alert, ACTION_CONNECT, ENDPOINT, ACTOR_ALERT_NAME, NULL);
-    zstr_sendx(nut_device_alert, ACTION_PRODUCER, FTY_PROTO_STREAM_ALERTS_SYS, NULL);
-    zstr_sendx(nut_device_alert, ACTION_CONSUMER, FTY_PROTO_STREAM_ASSETS, ".*", NULL);
 
     zstr_sendx(nut_sensor, ACTION_POLLING, polling, NULL);
-    zstr_sendx(nut_sensor, ACTION_CONNECT, ENDPOINT, ACTOR_SENSOR_NAME, NULL);
-    zstr_sendx(nut_sensor, ACTION_PRODUCER, FTY_PROTO_STREAM_METRICS_SENSOR, NULL);
-    zstr_sendx(nut_sensor, ACTION_CONSUMER, FTY_PROTO_STREAM_ASSETS, ".*", NULL);
 
     zpoller_t *poller = zpoller_new(nut_server, nut_device_alert, nut_sensor, NULL);
     assert(poller);

src/fty_nut_classes.h
@@ -33,6 +33,7 @@
 #include "../include/fty_nut.h"
 
 //  Extra headers
+#include "nut_mlm.h"
 
 //  Opaque class structures to allow forward references
 #ifndef LOGGER_T_DEFINED

commit 51babff19ac40cc1ae00b77db5b4af47041aecbb

Fix the alert actor mailbox client setup
I forgot to connect it.

Fixes: 96d189fa064f ("Simplify malamute connection setup")

src/alert_actor.cc
@@ -113,6 +113,10 @@ alert_actor (zsock_t *pipe, void *args)
        log_critical ("mlm_client_new () failed");
        return;
     }
+    if (mlm_client_connect(mb_client, endpoint, 5000, ACTOR_ALERT_MB_NAME) < 0) {
+        log_error("client %s failed to connect", ACTOR_ALERT_MB_NAME);
+        return;
+    }
 
     Devices devices(NutStateManager.getReader());
     devices.setPollingMs (polling);

src/nut_mlm.h
@@ -30,6 +30,7 @@
 #define MLM_ENDPOINT_VOID (const_cast<void *>(static_cast<const void *>(MLM_ENDPOINT)))
 #define ACTOR_NUT_NAME "fty-nut"
 #define ACTOR_ALERT_NAME "bios-nut-alert"
+#define ACTOR_ALERT_MB_NAME ACTOR_ALERT_NAME "-mb"
 #define ACTOR_SENSOR_NAME "agent-nut-sensor"
 
 #define CONFIG_POLLING "nut/polling_interval"